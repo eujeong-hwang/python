0c1bd81b6ec278b843a1fceac4a50f44db740084
eujeong-hwang <hj2bf@virginia.edu>
2022-03-08 23:22:29 +0900

    [March 08, 2022] feat: Method Overriding

--- Method Overriding.py
+++ Method Overriding.py
@@ -1,18 +1,31 @@
-# 다중 상속
+# 다중 상속 (Multiple Inheritance)
 # - 부모가 두 개 이상
+
+# Method Overriding
+from PIL import Image
+
+myImage = Image.open("class.PNG");
+
+myImage.show();
 
 # 일반 유닛
 class Unit: 
     # python constructor
     # https://www.geeksforgeeks.org/constructors-in-python/
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+    
+    def move(self, location):
+        print("[지상 유닛 이동]")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
+            .format(self.name, location, self.speed))
 
 # 공격 유닛 : Unit을 상속 받음
 class AttackUnit(Unit):
-    def __init__(self,name, hp, damage):
-        Unit.__init__(self,name,hp)
+    def __init__(self,name, hp,speed, damage):
+        Unit.__init__(self,name,hp, speed)
         self.damage = damage
     
     def attack(self, location):
@@ -28,15 +41,6 @@ class AttackUnit(Unit):
 
 # 메딕 : 의무병
 
-
-# 파이어뱃 : 공격 유닛, 화염방사기.
-firebat1 = AttackUnit("파이어뱃", 50 ,16)
-firebat1.attack("5시")
-
-# 공격 2번 받는다고 가정
-firebat1.damaged(25)
-firebat1.damaged(25)
-
 # 드랍쉽 : 공중 유닛, 수송기, 마린/ 파이어뱃/ 탱크 등을 수송, 공격 X
 
 # 날 수 있는 기능을 가진 클래스
@@ -51,12 +55,25 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 speed 0
         Flyable.__init__(self, flying_speed)
 
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+
 # 발키리: 공중 공격 유닛, 한 번에 14발 미사일 발사
-valkyrie = FlyableAttackUnit("valkyrie", 200, 6, 5)
+valkyrie = FlyableAttackUnit("Valkyrie", 200, 6, 5)
 valkyrie.fly(valkyrie.name, "3시")
 
-# Method Overriding
+# 벌쳐 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit("Vulture", 80, 10, 20)
+
+# 배틀그루저: 공중 유닛, 체력이 굉장히 좋음, 공격력도 좋음.
+battlecruiser = FlyableAttackUnit("BattleCruiser", 500, 25, 3)
+
+vulture.move("11시")
+# battlecruiser.fly(battlecruiser.name, "9시")
+battlecruiser.move("9시")
+
 
