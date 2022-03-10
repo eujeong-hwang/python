f882856ae89793abca9f2659e16a20ba08dad828
eujeong-hwang <hj2bf@virginia.edu>
2022-03-10 22:22:42 +0900

    [March 10, 2022] feat: StarCraft using Python

--- Project/StarCraft.py
+++ Project/StarCraft.py
@@ -1,3 +1,5 @@
+from random import *
+
 # 일반 유닛
 class Unit: 
     # python constructor
@@ -58,7 +60,7 @@ class Tank(AttackUnit):
             return
         
         # 현재 시즈모드가 아닐 때 -> 시즈모드
-        if self.seize == False:
+        if self.seize_mode == False:
             print("{0} : 시즈모드로 전환합니다.".format(self.name))
             self.damage *=2
             self.seize_mode = True
@@ -109,3 +111,54 @@ def game_start():
 def game_over():
     print("Player: gg") # good game
     print("[Player] 님이 게임에서 퇴장하셨습니다.")
+
+# 실제 게임 시작
+game_start()
+
+# 마린 3기 생성
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+# 탱크 2기 생성
+t1 = Tank()
+t2 = Tank()
+
+# 레이스 1기 생성
+w1 = Wraith()
+
+# 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 인동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seize_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+# 공격 모드 준비 ( 마린: 스팀팩, 탱크: 시즈모드, 레이스: 클로킹)
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack()
+    elif isinstance(unit, Tank):
+        unit.set_seize_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+# 전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5, 20)) # 공격은 랜덤으로 받음 (5 ~20)
+
+game_over()
