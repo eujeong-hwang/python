e98a787eb4e74ba42ed33036e0248ff944903ccb
eujeong-hwang <hj2bf@virginia.edu>
2022-03-17 21:36:43 +0900

    [March 17, 2022] feat: Data Structure

--- Data Structure/List.py
+++ Data Structure/List.py
@@ -1,32 +1,60 @@
 # List []
 
 # 지하철 칸 별로 10명, 20명, 30명
-subway = []
-subway1 = 10
-subway2 = 20
-subway3 = 30
+# subway = []
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
 
-subway.append(subway1)
-subway.append(subway2)
-subway.append(subway3)
-print(subway)
+# subway.append(subway1)
+# subway.append(subway2)
+# subway.append(subway3)
+# print(subway)
 
-subway =["유재석", "조세호", "박명수"]
+# subway =["유재석", "조세호", "박명수"]
 
-# 조세호씨가 몇 번째 칸에 탔나요?
-print(subway.index("조세호"))
+# # 조세호씨가 몇 번째 칸에 탔나요?
+# print(subway.index("조세호"))
 
-# 하하씨가 다음 정류장에서 다음 칸에 탐.
-subway.append("하하")
-print(subway)
+# # 하하씨가 다음 정류장에서 다음 칸에 탐.
+# subway.append("하하")
+# print(subway)
 
-# 정형돈을 유재석/ 조세호 사이에 태워봄
-subway.insert(1, "정형돈")
-print(subway)
+# # 정형돈을 유재석/ 조세호 사이에 태워봄
+# subway.insert(1, "정형돈")
+# print(subway)
 
-subway.insert(3, "황유정")
-print(subway)
+# subway.insert(3, "황유정")
+# print(subway)
 
 # 지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
-print(subway.pop())
-print(subway)
+# print(subway.pop())
+# print(subway)
+
+# 같은 이름의 사람이 몇 명 있는지 확인
+# subway.append("유재석")
+# print(subway)
+# print(subway.count("유재석"))
+
+# 정렬도 가능
+# num_list = [5,2,4,3,1]
+# num_list.sort()
+# print(num_list)
+
+# # 순서 뒤집기 가능
+# num_list.reverse()
+# print(num_list)
+
+# # 모두 지우기
+# num_list.clear()
+# print(num_list)
+
+
+# 다양한 자료형 함께 사용
+num_list =[5,2,4,3,1]
+mix_list = ["조세호", 20, True]
+print(mix_list)
+
+# 리스트 확장
+num_list.extend(mix_list)
+print(num_list)
